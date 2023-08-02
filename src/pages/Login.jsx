316715b7a29af3c97cd0d47f3beffc4cb619dc5c
maciekmalecki <mathumalecki@Mathus-MacBook-Pro.local>
2023-08-01 20:27:07 -0400

    Login skeleton complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './style.css';
+const dog = require('../assets/dog.jpg');
+const gitHubLogo = require('../assets/github.svg');
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='login'>
+      <img src={dog} width='100px' height='100px' alt='Svelte Logo' />
+      <h1>Login</h1>
+      <form>
+        <input placeholder='username'></input>
+        <br />
+        <input type='password' placeholder='password'></input>
+        <br />
+        <button className='loginButton'>Login</button>
+
+        <div className='oauthBox'>
+          <a className='outhButton'>
+            <button className='loginButton'>
+              Log in with GitHub
+              <img src={gitHubLogo} alt='gitHub Logo' />
+            </button>
+          </a>
+        </div>
+      </form>
     </div>
   );
 };
