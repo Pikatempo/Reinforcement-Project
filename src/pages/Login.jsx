8cea90b9b230fbdf9abd2b1a092d82506da4c866
maciekmalecki <mathumalecki@Mathus-MacBook-Pro.local>
2023-08-02 11:41:27 -0400

    Login and Signup Complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import './style.css';
 const dog = require('../assets/dog.jpg');
-const gitHubLogo = require('../assets/github.svg');
+import gitHubLogo from '../assets/git.png';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className='login'>
-      <img src={dog} width='100px' height='100px' alt='Svelte Logo' />
-      <h1>Login</h1>
-      <form>
-        <input placeholder='username'></input>
-        <br />
-        <input type='password' placeholder='password'></input>
-        <br />
-        <button className='loginButton'>Login</button>
+      <div className='loginCard'>
+        <div className='cardElements'>
+          {/* <img src={dog} width='100px' height='100px' alt='Svelte Logo' /> */}
+          <h1>Login</h1>
+          <form>
+            <input placeholder='username'></input>
+            <br />
+            <input type='password' placeholder='password'></input>
+            <br />
+            <Link to='/dashboard'>
+              <button className='loginButton'>Login</button>
+            </Link>
 
-        <div className='oauthBox'>
-          <a className='outhButton'>
-            <button className='loginButton'>
-              Log in with GitHub
-              <img src={gitHubLogo} alt='gitHub Logo' />
-            </button>
-          </a>
+            <div className='oauthBox'>
+              <a className='outhButton'>
+                <Link to='/dashboard'>
+                  <button className='loginButton'>Log in with GitHub</button>
+                </Link>
+              </a>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
